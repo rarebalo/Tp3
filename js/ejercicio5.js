d45ad836654bbad8b06691d8a07b62d2538d5634
Roberto <roberto.v.arebalo@gmail.com>
2023-11-10 20:56:11 -0300

    Se agrega el ejercicio 5 terminado

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -3,3 +3,31 @@
   A partir de la cadena que se le pasa, la función
    determina si esa cadena está formada sólo por
     mayúsculas, sólo por minúsculas o por una mezcla de ambas.*/
+
+function cadenaMayusculasOMinusculas(cadenaConsulta) {
+  let mayusculas;
+  let minusculas;
+
+  for (let index = 0; index < cadenaConsulta.length; index++) {
+    if (cadenaConsulta[index].toUpperCase() === cadenaConsulta[index]) {
+      mayusculas = true;
+    } else {
+      minusculas = true;
+    }
+
+    if (mayusculas && minusculas) {
+      return alert(
+        "Tu cadena de texto esta formada por una combinacion de mayusculas y minusculas"
+      );
+    }
+  }
+  if (mayusculas) {
+    return alert("Tu cadena de texto esta formada por mayusculas");
+  } else {
+    return alert("Tu cadena de texto esta formada por minusculas");
+  }
+}
+
+let cadenaDelUsuario = prompt("Ingrese una cadena de texto para verifica si esta compuesta por mayusculas, minusculas o una combinacio");
+
+cadenaMayusculasOMinusculas(cadenaDelUsuario);
