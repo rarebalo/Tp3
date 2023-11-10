e46aa049a1d8dc5af0bc3c7f3dfc2187bd528dc4
Roberto <roberto.v.arebalo@gmail.com>
2023-11-10 20:34:28 -0300

    Se agrega el ejercicio 4 terminado

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -2,3 +2,16 @@
  un número entero y devuelve como resultado una cadena de texto que
   indica si el número es par o impar. Mostrar por pantalla
    el resultado devuelto por la función.*/
+function seraPar(numeroQueConsulta) {
+  if (numeroQueConsulta % 2 === 0) {
+    return alert("Tu numero es par");
+  } else {
+    return alert("Tu numero no es par");
+  }
+}
+
+const numeroDelUsuario = parseInt(
+  prompt(`Ingrese un numero entero para comprobar si es par o no:`)
+);
+
+seraPar(numeroDelUsuario);
