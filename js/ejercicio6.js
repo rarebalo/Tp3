df8fe068c7717a2f330ac785baed4c1713669519
Roberto <roberto.v.arebalo@gmail.com>
2023-11-10 21:23:00 -0300

    Se agrega el ejercicio6 completo

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -13,3 +13,24 @@ Input:
 lado A = 24
 lado B = 5
 Output: 58    */
+
+function perimetroRectangulo(ladoA, ladoB) {
+  let perimetro = 2 * (ladoA + ladoB);
+
+  return perimetro;
+}
+
+let unLado;
+let otroLado;
+
+alert("Vamos a calcular el perimetro de un rectangulo");
+
+unLado = parseInt(prompt("Ingresa el lado mas corto del rectangulo"));
+otroLado = parseInt(prompt("Ingresa el lado mas largo del rectangulo"));
+
+if (unLado > otroLado) {
+    alert("Esta desobediencia a mis palabras me trae una profunda tristeza y si bien el lado mas corto es mas largo que el largo, lo vamos a calcular igual... ");
+} 
+alert(
+  `El perimetro de tu rectangulo es ${perimetroRectangulo(unLado, otroLado)}`
+);
