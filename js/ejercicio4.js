1c2ec1f6e794befb524698a2580a62634925a061
Roberto <roberto.v.arebalo@gmail.com>
2023-11-12 12:17:51 -0300

    Se agrega validaciones en el ejercicio4, no se logra validar si es un numero entero

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -13,5 +13,18 @@ function seraPar(numeroQueConsulta) {
 const numeroDelUsuario = parseInt(
   prompt(`Ingrese un numero entero para comprobar si es par o no:`)
 );
+if (Number.isNaN(numeroDelUsuario)) {
+  alert(" Em… tu ‘número’ no es número… por el momento sólo hemos sido provisto con la capacidad de determinar si es par o no de números.");
+} else {
+  seraPar(numeroDelUsuario);
+}
 
-seraPar(numeroDelUsuario);
+//Quiero validar que el usuario ingrese solamente numeros enteros, pero no lo logro, segun mi logica deberia funcionar de la siguiente manera
+
+/*if (Number.isNaN(numeroDelUsuario)) {
+  alert(" Em… tu ‘número’ no es número… por el momento sólo hemos sido provisto con la capacidad de determinar si es par o no de números.");
+} else if (!Number.isInteger(numeroDelUsuario)) {
+  alert("Em... tu 'número' no es entero...");
+} else {
+  seraPar(numeroDelUsuario);
+}*/
